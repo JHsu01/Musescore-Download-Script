c23cac926fda5eb490b829a2bc472b471ed9ce2d
anniegwong <45474744+anniegwong@users.noreply.github.com>
2020-03-07 02:02:47 -0500

    no significant changes

--- MuseScore Project/MuseScoreDownload.py
+++ MuseScore Project/MuseScoreDownload.py
@@ -1,23 +1,18 @@
-import requests
 import urllib.request
 
 if __name__ == '__main__':
     extensions = ['mp3', 'mxl', 'mid']
-    '''
     user_input = input("Search title: ")
     words = user_input.split(" ")
     query = "+".join(words)
     url = "https://musescore.com/sheetmusic?text=" + query
     print(url)
-    request = requests.get(url)
-    print(text)
-    '''
+    urllib.request.urlretrieve(url, 'C:/Users/Annie/Desktop/searchresults.png')
+
     pdf_url = 'https://musescore.com/score/2431571/download/pdf?h=12201921758843614765'
-    request = requests.get(pdf_url)
     pdf_address = 'C:/Users/Annie/Desktop/wearenumberone/score.pdf'
     urllib.request.urlretrieve(pdf_url, pdf_address)
 
-    page_number = 0
     for ext in extensions:
         url = 'https://musescore.com/static/musescore/scoredata/gen/1/7/5/2431571/2b880c7c3ccf9dfdc15f3a2b26f0377eb5ff1a20/score.'
         file_url = url + ext
