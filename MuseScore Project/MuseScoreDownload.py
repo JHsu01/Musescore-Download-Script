114b8940e26702a51c925383c249584312319fee
anniegwong <45474744+anniegwong@users.noreply.github.com>
2020-03-07 00:59:53 -0500

    updated to permanent pdf url

--- MuseScore Project/MuseScoreDownload.py
+++ MuseScore Project/MuseScoreDownload.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     request = requests.get(url)
     print(text)
     '''
-    pdf_url = 'https://s3.ultimate-guitar.com/musescore.scoredata/gen/1/7/5/2431571/2b880c7c3ccf9dfdc15f3a2b26f0377eb5ff1a20/score_full.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=4SHNMJS3MTR9XKK7ULEP%2F20200307%2Fus-west%2Fs3%2Faws4_request&X-Amz-Date=20200307T035533Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Signature=f2690a33277af1205ea246e5c7b2cac3006c54173a08fb0a9dc17ce45f85e015'
+    pdf_url = 'https://musescore.com/score/2431571/download/pdf?h=12201921758843614765'
     request = requests.get(pdf_url)
     pdf_address = 'C:/Users/Annie/Desktop/wearenumberone/score.pdf'
     urllib.request.urlretrieve(pdf_url, pdf_address)
